25a14d67273da26d213138bb6002aa4d22421d36
Peter Georgeson <peter@supernifty.org>
2019-06-10 21:39:19 +1000

    add sd to summary

--- csvsummary.py
+++ csvsummary.py
@@ -8,9 +8,11 @@ import csv
 import logging
 import sys
 
+import numpy
+
 def check(summary, col):
   if col not in summary:
-    summary[col] = {'n': 0, 'sum': 0, 'max': sys.float_info.min, 'min': sys.float_info.max}
+    summary[col] = {'n': 0, 'sum': 0, 'max': sys.float_info.min, 'min': sys.float_info.max, 'd': []}
   return summary
 
 def main(colnames, delimiter, fh, out):
@@ -30,15 +32,20 @@ def main(colnames, delimiter, fh, out):
       summary[col]['sum'] += v
       summary[col]['max'] = max(summary[col]['max'], v)
       summary[col]['min'] = min(summary[col]['min'], v)
+      summary[col]['d'].append(v)
 
   # write summary
-  out.write('name\tn\ttotal\tmin\tmax\tmean\n')
+  out.write('name\tn\ttotal\tmin\tmax\tmean\tsd\n')
   for col in colnames:
     if summary[col]['n'] > 0:
       summary[col]['mean'] = summary[col]['sum'] / summary[col]['n']
-      out.write('{}\t{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\n'.format(col, summary[col]['n'], summary[col]['sum'], summary[col]['min'], summary[col]['max'], summary[col]['mean']))
+      if summary[col]['n'] > 1:
+        summary[col]['sd'] = numpy.std(summary[col]['d'])
+      else:
+        summary[col]['sd'] = 0
+      out.write('{}\t{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\n'.format(col, summary[col]['n'], summary[col]['sum'], summary[col]['min'], summary[col]['max'], summary[col]['mean'], summary[col]['sd']))
     else:
-      out.write('{}\t0\t0\t-\t-\t-\n'.format(col))
+      out.write('{}\t0\t0\t-\t-\t-\t-\n'.format(col))
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Assess MSI')
